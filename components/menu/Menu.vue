b3a1bb3530b0d7ff956478657552ec68501ab94e
Paolo <paolo.mangia@superscommesse.it>
2020-05-25 23:07:53 +0200

    menu

--- components/menu/Menu.vue
+++ components/menu/Menu.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="navigation container fixed left-0 right-0 m-auto z-50">
-    <button class="absolute navigation__button btn btn-transparent right-0 mt-8 flex justify-center items-center outline-none">
+  <div class="nav-menu" :class="{ open: menuOpen }">
+    <!-- <button class="absolute navigation__button btn btn-transparent right-0 mt-8 flex justify-center items-center outline-none">
       <span class="navigation__icon flex justify-center items-center">&nbsp;</span>
     </button>
 
@@ -8,12 +8,16 @@
       <div class="navigation__background">
         &nbsp;
       </div>
+    </div> -->
+    <div class="menu-backdrop fixed left-0 top-0 w-screen h-screen" />
+    <div class="container fixed left-0 right-0 m-auto z-50">
+      <div class="expandable-pentagon" />
+      <div class="menu-toggle" @click="openMenu">
+        <span class="menu-toggle_icon">&nbsp;</span>
+      </div>
     </div>
 
-    <!-- <nav class="navigation__nav">
-      <div class="navigation__blurspace">
-        &nbsp;
-      </div>
+    <nav class="navigation__nav">
       <ul class="navigation__list">
         <li class="navigation__item">
           <a id="nav__list--signup" class="navigation__link">Registrati</a>
@@ -31,13 +35,32 @@
           <a id="nav__list--contacts" class="navigation__link">Contatti</a>
         </li>
       </ul>
-    </nav> -->
+    </nav>
   </div>
 </template>
 
 <script>
 import './Menu.sass'
 export default {
-
+  data () {
+    return {
+      menuOpen: false
+    }
+  },
+  watch: {
+    /* menuOpen (val) {
+      const body = document.body
+      if (val) {
+        body.style.overflow = 'hidden'
+      } else {
+        body.style.overflow = 'auto'
+      }
+    } */
+  },
+  methods: {
+    openMenu () {
+      this.menuOpen = !this.menuOpen
+    }
+  }
 }
 </script>
